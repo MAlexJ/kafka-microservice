71200983a914bc40f159856017a288c250f78a62
malexj <alexmaximovj@gmail.com>
2024-08-15 10:43:41 +0300

    add items_processor_service

---
+++ items_processor_service/src/main/java/com/malex/items_processor_service/ItemsProcessorServiceApplication.java
@@ -0,0 +1,12 @@
+package com.malex.items_processor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsProcessorServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ItemsProcessorServiceApplication.class, args);
+  }
+}
---
+++ items_processor_service/src/test/java/com/malex/items_processor_service/ItemsProcessorServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.malex.items_processor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsProcessorServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
